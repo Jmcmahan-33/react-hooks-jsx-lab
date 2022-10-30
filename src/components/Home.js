2ef94f9663f482b3d2805cf1e819c338aebeddbd
Jared McMahan <jaredm788@gmail.com>
2022-10-30 14:21:09 -0600

    commit tree edd0b8df63b742e1c451cbd8c5dc028ada5fe48e

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { name, city } from "../data/data.js";
-
+const headerStyle = {
+  color: 'firebrick'
+}
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id ="home">
+      <h1 style = {headerStyle}>{name} is a Web Developer from {city}</h1>
+    
+    </div>
+
+  )
 }
 
 export default Home;
+
+
+
+// ✕ the h1 has a an inline style attribute with a color of 'firebrick' (4 ms)
+
+
